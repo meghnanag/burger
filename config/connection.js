5c2b3b518af257ae0851b0675b55a1c92742a9fb
Meghna <meghnanag16@gmail.com>
2018-05-05 07:49:04 -0700

    jaws db issue

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'Destiny11*',
+    password : '',
     database : 'burgers_db'
   });
 };
